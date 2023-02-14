07bf52f3195e95e1b12c87befd5fca8fe65b84d5
Daniel <anikindaniel2007@gmail.com>
2023-02-15 00:29:04 +0300

    add Debugging menu(smth)

--- GUI.py
+++ GUI.py
@@ -4,11 +4,13 @@ import serial
 from PyQt5.QtWidgets import QApplication, QWidget, QTableWidgetItem, QInputDialog, QSlider
 from PyQt5.QtGui import QImage, QPalette, QBrush
 from MainWidget import Ui_Main_Screen
+from DebuggingWidget import Ui_Debugging_menu_screen
 
 
 class MainScreen(QWidget, Ui_Main_Screen):
     def __init__(self):
         super().__init__()
+        self.debug_widget = DebuggingMenu()
         self.setupUi(self)
         self.setWindowTitle('Main screen')
         self.connecting_btns()
@@ -22,16 +24,30 @@ class MainScreen(QWidget, Ui_Main_Screen):
         self.Stop_Btn.clicked.connect(self.stop)
 
     def start(self):
-        self.ser.write(b"Start_Btn.clicked = 1\n")
+        self.ser.write(b"0x00\n")
 
     def infinity(self):
-        self.ser.write(b"Infinity_Btn.clicked = 1\n")
+        self.ser.write(b"0x01\n")
 
     def stop(self):
-        self.ser.write(b"Stop_Btn.clicked = 1\n")
+        self.ser.write(b"0x02\n")
 
     def debugging_menu(self):
-        pass
+        self.close()
+        self.debug_widget.show()
+
+
+class DebuggingMenu(QWidget, Ui_Debugging_menu_screen):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.connecting_btns()
+        
+    def connecting_btns(self):
+        self.Debug_Btn.clicked.connect(self.debugging_menu)
+        self.Start_Btn.clicked.connect(self.start)
+        self.Infinity_Btn.clicked.connect(self.infinity)
+        self.Stop_Btn.clicked.connect(self.stop)
 
 
 if __name__ == '__main__':
