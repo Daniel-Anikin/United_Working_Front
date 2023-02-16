bf3b2cb3ec72fe9c548c0b3881d38c7c55e6c71a
Daniel <anikindaniel2007@gmail.com>
2023-02-16 14:33:33 +0300

    one extra commit

--- GUI.py
+++ GUI.py
@@ -10,7 +10,6 @@ from DebuggingWidget import Ui_Debugging_menu_screen
 class MainScreen(QWidget, Ui_Main_Screen):
     def __init__(self):
         super().__init__()
-        self.debug_widget = DebuggingMenu()
         self.setupUi(self)
         self.setWindowTitle('Main screen')
         self.connecting_btns()
@@ -24,15 +23,19 @@ class MainScreen(QWidget, Ui_Main_Screen):
         self.Stop_Btn.clicked.connect(self.stop)
 
     def start(self):
+        pass
         self.ser.write(b"0x00\n")
 
     def infinity(self):
+        pass
         self.ser.write(b"0x01\n")
 
     def stop(self):
+        pass
         self.ser.write(b"0x02\n")
 
     def debugging_menu(self):
+        self.debug_widget = DebuggingMenu()
         self.close()
         self.debug_widget.show()
 
@@ -41,13 +44,40 @@ class DebuggingMenu(QWidget, Ui_Debugging_menu_screen):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
         self.connecting_btns()
         
     def connecting_btns(self):
-        self.Debug_Btn.clicked.connect(self.debugging_menu)
-        self.Start_Btn.clicked.connect(self.start)
-        self.Infinity_Btn.clicked.connect(self.infinity)
         self.Stop_Btn.clicked.connect(self.stop)
+        self.Rotate_Btn.clicked.connect(self.rotate)
+        self.CSV_Btn.clicked.connect(self.CSV)
+        self.Lift_Up_Btn.clicked.connect(self.lift_up)
+        self.Lift_Down_Btn.clicked.connect(self.lift_down)
+        self.Load_Btn.clicked.connect(self.load)
+        self.Undo_Btn.clicked.connect(self.undo)
+
+    def stop(self):
+        self.ser.write(b"0x02\n")
+
+    def rotate(self):
+        self.ser.write(b"0x10\n")
+
+    def CSV(self):
+        self.ser.write(b"0x11\n")
+
+    def lift_up(self):
+        self.ser.write(b"0x12\n")
+
+    def lift_down(self):
+        self.ser.write(b"0x13\n")
+
+    def load(self):
+        self.ser.write(b"0x14\n")
+
+    def undo(self):
+        self.main_widget = MainScreen()
+        self.close()
+        self.main_widget.show()
 
 
 if __name__ == '__main__':
